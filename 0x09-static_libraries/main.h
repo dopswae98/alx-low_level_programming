3115653e30ffe25c66024e7839e9c3822725052a
dopswae98 <ephraimmatarutse@gmail.com>
2023-01-06 15:27:37 -0800

    static

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,7 +1,6 @@
-#ifndef FILE_MAIN
-#define FILE_MAIN
-
-/*copy list from task*/
+#ifndef MAIN_H
+#define MAIN_H
+#include <stddef.h>
 
 int _putchar(char c);
 int _islower(int c);
@@ -23,6 +22,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
 #endif
-
